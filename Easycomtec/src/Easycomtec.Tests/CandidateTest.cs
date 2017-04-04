df0ada48cd2df0b608b1660d9b5ac9590fd86497
Edney Batista da Silva <edney_contato@hotmail.com>
2017-04-04 13:30:27 -0300

    Add files via upload

---
+++ Easycomtec/src/Easycomtec.Tests/CandidateTest.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easycomtec.Tests
+{
+    public class CandidateTest : IDisposable
+    {
+        IWebDriver Driver;
+        public CandidateTest()
+            : base()
+        {
+            var capabilities = DesiredCapabilities.Chrome();
+            Driver = new RemoteWebDriver(new Uri("http://localhost:9515"), capabilities);
+        }
+
+
+        public CandidateTest GoToForms()
+        {
+            Driver.Navigate().GoToUrl("http://localhost:57720/Home/Login");
+            return this;
+        }
+
+        public CandidateTest CreateNewAccount()
+        {
+            Driver.FindElement(By.LinkText("create account")).Click();
+            return this;
+        }
+
+
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+
+    }
+}
---
+++ Easycomtec/src/Easycomtec.Tests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easycomtec.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (CandidateTest test = new CandidateTest())
+            {
+                test.GoToForms().CreateNewAccount();
+            }
+        }
+    }
+}
