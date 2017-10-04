2c3faf5a713b6a205f82da0b794c3ae35c0a4d36
Edney Batista da Silva <edney_contato@hotmail.com>
2017-10-03 23:37:47 -0300

    Add files via upload

--- Easycomtec/src/Easycomtec/Controllers/CandidateController.cs
+++ Easycomtec/src/Easycomtec/Controllers/CandidateController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Easycomtec.Lib;
+using Easycomtec.Extensoes;
+using Easycomtec.Lib.Extension;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,26 +28,121 @@ namespace Easycomtec.Controllers
         {
             return View();
         }
-        
+
+        [Route("Candidate/{id}")]
+        public IActionResult Candidate(int id)
+        {
+            var candidate = EasyRepository.Query<Candidate>()
+                .Property((c) => c.Account)
+                .Property((c) => c.Skills)
+                .Property((c) => c.Phones)
+                .Property((c) => c.Address)
+                .FirstOrDefault(w => w.Id == id);
+            return View(candidate);
+        }
+
+        [Route("Candidates")]
+        public IActionResult Candidates()
+        {
+            return View(EasyRepository.Query<Candidate>().Property((c) => c.Account).ToList());
+        }
+
+        [Route("Candidate/AddPhone")]
+        [HttpPost]
+        public JsonResult AddPhone([FromBody]Phone phone)
+        {
+            try
+            {
+                EasyRepository.AddOrUpdate(phone).Save();
+                return this.AsSuccessJsonResult(phone);
+            }
+            catch (Exception ex)
+            {
+                return ex.AsJsonResult();
+            }
+        }
+
+        [Route("Candidate/RemovePhone")]
+        [HttpPost]
+        public JsonResult RemovePhone([FromBody]Phone phone)
+        {
+            try
+            {
+                EasyRepository.RemoveItem(phone).Save();
+                return this.AsSuccessJsonResult(phone);
+            }
+            catch (Exception ex)
+            {
+                return ex.AsJsonResult();
+            }
+        }
+
+        [Route("Candidate/AddSkill")]
+        [HttpPost]
+        public JsonResult AddSkill([FromBody]Skill skill)
+        {
+            try
+            {
+                EasyRepository.AddOrUpdate(skill).Save();
+                return this.AsSuccessJsonResult(skill);
+            }
+            catch (Exception ex)
+            {
+                return ex.AsJsonResult();
+            }
+        }
+
+        [Route("Candidate/RemoveSkill")]
+        [HttpPost]
+        public JsonResult RemoveSkill([FromBody]Skill skill)
+        {
+            try
+            {
+                EasyRepository.RemoveItem(skill).Save();
+                return this.AsSuccessJsonResult();
+            }
+            catch (Exception ex)
+            {
+                return ex.AsJsonResult();
+            }
+        }
+
         public IActionResult PersonalData()
         {
             return View();
         }
-                
+
         [HttpPost]
         public JsonResult Create([FromBody]Candidate candidate)
         {
-            if (!this.ModelState.IsValid) ;
-            object retorno = null;
+            if (!this.ModelState.IsValid)
+                return this.ModelState.AsJsonResult();
             try
             {
                 EasyRepository.AddOrUpdate(candidate).Save();
+                return this.AsSuccessJsonResult();
             }
             catch (Exception ex)
             {
+                return ex.AsJsonResult();
             }
-            return Json(retorno);
-            //return View();
+        }
+
+        [Route("Candidate/Edit")]
+        [HttpPost]
+        public JsonResult Edit([FromBody]Candidate candidate)
+        {
+            if (!this.ModelState.IsValid)
+                return this.ModelState.AsJsonResult();
+            try
+            {
+                EasyRepository.AddOrUpdate(candidate).Save();
+                return this.AsSuccessJsonResult();
+            }
+            catch (Exception ex)
+            {
+                return ex.AsJsonResult();
+            }
         }
     }
 }
